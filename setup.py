7c5987a6bae3bc1cf9eb527fb4edc103923715cc
Michael Pearmain <michael.pearmain@gmail.com>
2017-02-27 10:45:28 +0000

    Adding package setup files

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gestalt',
+    version='0.1.0',
+    url='https://github.com/mpearmain/gestalt',
+    packages=find_packages(),
+    description='Data science pipeline automation',
+    install_requires=[
+        "numpy >= 1.11.1",
+        "pandas >= 0.19.0",
+        "scipy >= 0.18.1",
+        "scikit-learn >= 0.18.0",
+    ],
+)
