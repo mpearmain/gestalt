f0c99052b4463fdbc31d8c172e771ded564f43da
Mike Pearmain <michael.pearmain@gmail.com>
2017-04-27 11:05:39 +0100

    version bump for minor fixes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gestalt',
-    version='0.1.0',
+    version='0.1.1',
     url='https://github.com/mpearmain/gestalt',
     packages=find_packages(),
     description='Data science pipeline automation',
